940e7f9de2abf323b75c6dfd4dcea6b08ce1c7b8
daniely2421 <trindadedaniely21@gmail.com>
2025-03-20 21:11:25 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'jogo adivinhação';              
+
+let subtitulo= document.querySelector('p');
+subtitulo.innerHTML = 'escolha um numero de 1 a 100';              
+
+Function verificarChute() { 
+    alert ('apertou o botão')
+}let campo =document.querySelector(tag); 
+campo.innerHTML =texto
